32666175e4bd6f751fd13d0016ffe9f51730b4e8
Ryan Ramage <ryan.ramage@gmail.com>
2014-02-12 19:44:27 -0700

    rename tests

--- tests/test.js
+++ tests/test.js
@@ -11,13 +11,13 @@ test('symetric streaming encryption', function(t){
   t.plan(1);
 
   fs.createReadStream("./tests/assets/secret.txt")
-    .pipe(new xxtea.Encrypt('8339d93jdooe2dwd', 'utf8'))
+    .pipe(new xxtea.Encrypt(bops.from('8339d93jdooe2dwd', 'utf8')))
     .pipe(fs.createWriteStream('out.text.tea'))
     .on('close', decrypt);
 
   function decrypt(){
     fs.createReadStream("./out.text.tea")
-      .pipe(new xxtea.Decrypt('8339d93jdooe2dwd', 'utf8'))
+      .pipe(new xxtea.Decrypt(bops.from('8339d93jdooe2dwd', 'utf8')))
       .pipe(concat(function(contents) {
         console.log(contents.toString('utf-8'));
         t.equal(contents.toString('utf-8'), 'Something secret should not be seen. Very amaze.')
