1159d8cd6076b891af2aaf84da735bb9ed929004
Ryan Ramage <ryan.ramage@gmail.com>
2014-02-12 17:02:28 -0700

    better testing

--- tests/test.js
+++ tests/test.js
@@ -1,27 +1,27 @@
 var xxtea = require('../'),
+    test = require('tape'),
     fs = require('fs'),
     bops = require('bops');
 var concat = require('concat-stream');
 
-fs.createReadStream("./tests/assets/secret.txt")
-  .pipe(new xxtea.Encrypt('8339d93jdooe2dwd', 'utf8'))
-  .pipe(fs.createWriteStream('out.text.tea'))
-  .on('end', decrypt);
-
-setTimeout(decrypt, 2000);
-
-function decrypt(){
-  console.log('about to decryt');
-  fs.createReadStream("./out.text.tea")
-    .pipe(new xxtea.Decrypt('8339d93jdooe2dwd', 'utf8'))
-    .pipe(concat(function(contents) {
-      console.log(contents.toString('utf-8'));
-      // contents is the contents of the entire file
-    }))
-
-}
 
 
+test('symetric streaming encryption', function(t){
 
+  t.plan(1);
+
+  fs.createReadStream("./tests/assets/secret.txt")
+    .pipe(new xxtea.Encrypt('8339d93jdooe2dwd', 'utf8'))
+    .pipe(fs.createWriteStream('out.text.tea'))
+    .on('close', decrypt);
+
+  function decrypt(){
+    fs.createReadStream("./out.text.tea")
+      .pipe(new xxtea.Decrypt('8339d93jdooe2dwd', 'utf8'))
+      .pipe(concat(function(contents) {
+        t.equal('Something secret should not be seen. Very amaze.', contents.toString('utf-8'))
+      }))
+  }
+})
 
 
